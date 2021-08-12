57b7a38de246c6dfc68d270791dc1fa1213f2582
Athos Roman <athosgroman@gmail.com>
2021-08-11 21:24:15 -0300

    Add menu

--- src/agendamento/FuncoesAgendamento.java
+++ src/agendamento/FuncoesAgendamento.java
@@ -2,6 +2,14 @@ package agendamento;
 
 public class FuncoesAgendamento {
 
-	//Aqui devem ser criadas as funções utilizadas no arquivo SistemaAgendamento
+	public static void exibirMenu() {
+        System.out.println("PROGRAMA AGENDAMENTO");
+        System.out.println("Escolha sua opção!");
+        System.out.println("1 - Digitar horarios disponíveis para agendamento");
+        System.out.println("2 - Agendar cliente");
+        System.out.println("3 - Exibir agenda");
+        System.out.println("4 - Desligar programa");
+
+    }
 
 }
